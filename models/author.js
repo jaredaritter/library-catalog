98a5dde263c32bfe950075166a621b80260f6591
jared <38332939+jaredaritter@users.noreply.github.com>
2020-09-01 19:03:55 -0400

    Added return to virtual allowing name to work correctly

--- models/author.js
+++ models/author.js
@@ -14,13 +14,14 @@ AuthorSchema.virtual('name').get(function () {
   // To avoid errors in cases where an author does not have either a family name or first name
   // We want to make sure we handle the exception by returning an empty string for that case
 
-  const fullname = '';
+  let fullname = '';
   if (this.first_name && this.family_name) {
     fullname = this.family_name + ', ' + this.first_name;
   }
   if (!this.first_name || !this.family_name) {
     fullname = '';
   }
+  return fullname;
 });
 
 // Virtual for author's lifespan
