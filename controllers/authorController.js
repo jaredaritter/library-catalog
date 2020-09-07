9cdc6c29712ae44c24893eb2408006ca2e5d32bb
jared <38332939+jaredaritter@users.noreply.github.com>
2020-09-07 11:39:11 -0400

    Add author form page. Add get and post logic for author form page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require('../models/author');
 const book = require('../models/book');
 const async = require('async');
+const { body, validationResult } = require('express-validator');
 
 // DISPLAY LIST OF ALL AUTHORS
 exports.author_list = (req, res) => {
@@ -49,14 +50,61 @@ exports.author_detail = (req, res, next) => {
 };
 
 // DISPLAY AUTHOR CREATE FORM ON GET
-exports.author_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create get');
+exports.author_create_get = (req, res, next) => {
+  res.render('author_form', { title: 'Create Author' });
 };
 
 // HANDLE AUTHOR CREATE ON POST
-exports.author_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+  // Validate and sanitize
+  body('first_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.')
+    .escape(),
+  body('family_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.')
+    .escape(),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  // Process request
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('author_form', {
+        title: 'Create Author',
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(author.url);
+      });
+    }
+  },
+];
 
 // DISPLAY AUTHOR DELETE FORM ON GET
 exports.author_delete_get = (req, res) => {
