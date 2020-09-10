76e746c6f1f2305d9161435d09dc2bd3a6661d28
jared <38332939+jaredaritter@users.noreply.github.com>
2020-09-10 14:36:47 -0400

    Add Update link to bookinstance details page. Add GET and POST controller logic to bookinstance. Working properly outside of convenience issues with form

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,6 +1,7 @@
 var BookInstance = require('../models/bookinstance');
 var Book = require('../models/book');
 const { body, validationResult } = require('express-validator');
+const async = require('async');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function (req, res, next) {
@@ -134,11 +135,81 @@ exports.bookinstance_delete_post = function (req, res, next) {
 };
 
 // Display BookInstance update form on GET.
-exports.bookinstance_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance update GET');
+exports.bookinstance_update_get = function (req, res, next) {
+  async.parallel(
+    {
+      bookinstance: function (callback) {
+        BookInstance.findById(req.params.id).exec(callback);
+      },
+      book_list: function (callback) {
+        Book.find({}, 'title').exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.bookinstance == null) {
+        const err = new Error('Book Instance not found');
+        err.status = 404;
+        return next(err);
+        // Can probably also just redirect (could put error message on redirected page)
+      }
+      res.render('bookinstance_form', {
+        title: 'Update Book Instance',
+        bookinstance: results.bookinstance,
+        book_list: results.book_list,
+      });
+    }
+  );
 };
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance update POST');
-};
+exports.bookinstance_update_post = [
+  // Validate and sanitize
+  body('book', 'Book must not be empty').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('due_back', 'Date must be valid')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('status', 'Status must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      due_back: req.body.due_back,
+      status: req.body.status,
+      _id: req.params.id, // Required or a new ID will be assigned!
+    });
+    if (!errors.isEmpty()) {
+      Book.find({}, 'title').exec(function (err, book_list) {
+        if (err) {
+          return next(err);
+        }
+        res.render('bookinstance_form', {
+          title: 'Update Book Instance',
+          book_list: book_list,
+          bookinstance: bookinstance,
+          errors: errors.array(),
+        });
+        return;
+      });
+    } else {
+      BookInstance.findByIdAndUpdate(req.params.id, bookinstance, {}, function (
+        err,
+        thebookinstance
+      ) {
+        if (err) return next(err);
+        res.redirect(thebookinstance.url);
+      });
+    }
+  },
+];
